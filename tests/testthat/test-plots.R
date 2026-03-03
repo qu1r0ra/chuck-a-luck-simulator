4aaf5520ec81f03b62c0eeb475c64d6e2bfd7926
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2026-03-04 01:11:47 +0800

    test: add plot tests

---
+++ tests/testthat/test-plots.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+source("../../R/constants.R")
+source("../../R/statistics.R")
+source("../../R/plots.R")
+
+test_that("LLN plot produces a ggplot object", {
+  sim <- data.frame(Round = 1:10, Matches = sample(0:3, 10, replace = TRUE))
+  p <- plot_lln_convergence(sim, 2)
+  expect_s3_class(p, "ggplot")
+})
+
+test_that("LLN plot handles empty data", {
+  p <- plot_lln_convergence(data.frame(), 2)
+  expect_s3_class(p, "ggplot")
+})
+
+test_that("Distribution plot produces a ggplot object", {
+  sim <- data.frame(Matches = sample(0:3, 100, replace = TRUE))
+  p <- plot_outcome_distribution(sim)
+  expect_s3_class(p, "ggplot")
+})
+
+test_that("Distribution plot handles empty data", {
+  p <- plot_outcome_distribution(data.frame())
+  expect_s3_class(p, "ggplot")
+})
